9ec98a08583b6a69d3c83924bc97fb2c0bc43dc7
bentomka <144691335+bentomka@users.noreply.github.com>
2026-04-02 12:47:40 -0400

    Updated with statified k fold cross validation

--- train_randomforest.py
+++ train_randomforest.py
@@ -6,6 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import StratifiedKFold, cross_val_predict
 
 FEATURE_FILE = "audio_features.csv"
 df = pd.read_csv(FEATURE_FILE)
@@ -14,41 +15,28 @@ def run_model(sub_df, label):
     # exclude non-numeric columns for ML
     exclude_cols = ["filename", "sound_class", "disease_class", "patient_id"]
     X = sub_df.drop(columns=exclude_cols).values
-    y = sub_df["sound_class"].values
-    patient_ids = sub_df["patient_id"].values
-
-    # Patient-level split
-    # ensure same patient doesn't appear in train and test
-    unique_patients = sub_df["patient_id"].unique()
-    train_patients, test_patients = train_test_split(unique_patients, test_size=0.2, random_state=42)
-
-    train_idx = sub_df["patient_id"].isin(train_patients)
-    test_idx = sub_df["patient_id"].isin(test_patients)
-
-    X_train, X_test = X[train_idx], X[test_idx]
-    y_train, y_test = y[train_idx], y[test_idx]
+    y = sub_df["disease_class"].values
 
     # Feature scaling
     scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
+    X_scaled = scaler.fit_transform(X)
 
     # Train Random Forest
     rf = RandomForestClassifier(n_estimators=200, max_depth=None, class_weight="balanced", random_state=42)
 
-    rf.fit(X_train, y_train)
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
     # Evaluate
-    y_pred = rf.predict(X_test)
+    y_pred = cross_val_predict(rf, X_scaled, y, cv=skf)
 
     print("Classification Report:")
-    print(classification_report(y_test, y_pred))
+    print(classification_report(y, y_pred))
 
     # Confusion matrix
-    cm = confusion_matrix(y_test, y_pred, labels=rf.classes_)
+    cm = confusion_matrix(y, y_pred, labels=np.unique(y))
 
     plt.figure(figsize=(8,6))
-    sns.heatmap(cm, annot=True, fmt="d", xticklabels=rf.classes_, yticklabels=rf.classes_, cmap="Blues")
+    sns.heatmap(cm, annot=True, fmt="d", xticklabels=np.unique(y), yticklabels=np.unique(y), cmap="Blues")
 
     plt.xlabel("Predicted")
     plt.ylabel("True")
