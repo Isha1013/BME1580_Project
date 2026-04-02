d1fdeb3d04a0e45ee6ccdf4ce24ff58aab08b3e0
bentomka <144691335+bentomka@users.noreply.github.com>
2026-04-02 12:47:09 -0400

    Updated with statified k fold cross validation

--- train_knn.py
+++ train_knn.py
@@ -6,6 +6,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import StratifiedKFold, cross_val_predict
 
 FEATURE_FILE = "audio_features.csv"
 df = pd.read_csv(FEATURE_FILE)
@@ -13,46 +14,32 @@ df = pd.read_csv(FEATURE_FILE)
 def run_model(sub_df, label):
     print(f"\n Running model: {label}") 
 
-
     # exclude non-numeric columns for ML
     exclude_cols = ["filename", "sound_class", "disease_class", "patient_id"]
     X = sub_df.drop(columns=exclude_cols).values
-    y = sub_df["sound_class"].values
-
-    patient_ids = sub_df["patient_id"].values
-
-    # Patient-level split
-    # ensure same patient doesn't appear in train and test
-    unique_patients = sub_df["patient_id"].unique()
-    
-    train_patients, test_patients = train_test_split(unique_patients, test_size=0.2, random_state=42)
-
-    train_idx = sub_df["patient_id"].isin(train_patients)
-    test_idx = sub_df["patient_id"].isin(test_patients)
-
-    X_train, X_test = X[train_idx], X[test_idx]
-    y_train, y_test = y[train_idx], y[test_idx]
+    y = sub_df["disease_class"].values
 
     # Feature scaling
     scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
+    X_scaled = scaler.fit_transform(X)
 
     # Train KNN
     knn = KNeighborsClassifier(n_neighbors=5)
-    knn.fit(X_train, y_train)
+    
+    # Stratified K-Fold Cross Validation
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
-    # Evaluate
-    y_pred = knn.predict(X_test)
+    #Evaluate
+    y_pred = cross_val_predict(knn, X_scaled, y, cv=skf)
 
     print("Classification Report:")
-    print(classification_report(y_test, y_pred))
+    print(classification_report(y, y_pred))
 
     # Confusion matrix
-    cm = confusion_matrix(y_test, y_pred, labels=knn.classes_)
+    cm = confusion_matrix(y, y_pred, labels=np.unique(y))
 
     plt.figure(figsize=(8,6))
-    sns.heatmap(cm, annot=True, fmt="d", xticklabels=knn.classes_, yticklabels=knn.classes_, cmap="Blues")
+    sns.heatmap(cm, annot=True, fmt="d", xticklabels=np.unique(y), yticklabels=np.unique(y), cmap="Blues")
 
     plt.xlabel("Predicted")
     plt.ylabel("True")
